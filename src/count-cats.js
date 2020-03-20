a3db2ec5148fbd01317ded34cbd9cbf628bb5089
Kazak Dmitry <kazak.dmitry1989@mail.ru>
2020-03-20 21:06:55 +0300

    count-cat

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,21 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+
+
+
+
+
+
+module.exports = function countCats(matrix) {
+   var counter=0;
+   for(var i=0;i<matrix.length;i++){
+	   
+	   if(Array.isArray(matrix[i])){
+		   for(var k=0;k < matrix[i].length; k++){
+				if (matrix[i][k]=='^^'){
+					counter += 1;
+				}  
+		   }
+	   }
+   }
+   return counter;
 };
+
