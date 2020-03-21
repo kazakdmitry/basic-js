4410c40ab15d11bd7f8153ff304625bf2cbe38a5
Kazak Dmitry <kazak.dmitry1989@mail.ru>
2020-03-21 19:45:11 +0300

    feat:dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,27 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports =  function createDreamTeam(str) {
+	if(str==null){return false;}
+	var str1=[];
+	 var arr=[];
+	var arr1;
+	
+	
+	for(var k=0;k<str.length;k++){
+		if(Array.isArray(str[k])){
+			
+			continue;
+		}
+		if(typeof(str[k])=="object"){
+			continue;
+		}
+		
+		if (typeof(str[k])=='string'){
+			var s=str[k].trim();
+			if(str[k].match(/[a-zA-Z]/)){
+				var t=s[0].toUpperCase();
+				arr.push(t);
+			}
+		}
+	}
+	
+	return arr.sort().join('');
 };
